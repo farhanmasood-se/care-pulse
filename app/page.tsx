4202852b546e32a8fb68244b95d8d6c2659edf98
Farhan Masood <farhan.masood@mslm.io>
2024-07-16 12:52:58 +0500

    starter

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image />
+        </div>
+      </section>
+    </div>
+  );
+}
